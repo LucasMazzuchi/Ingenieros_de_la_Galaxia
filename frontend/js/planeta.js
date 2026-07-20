0db3c5bf0d247b07a9519c92f9707fee68824028
Mariano Verruno <mverruno@fi.uba.ar>
2026-07-20 20:36:10 +0000

    Subo cambios a los moldes de planeta, interios y retoques en la caratula de presentacion

--- frontend/js/planeta.js
+++ frontend/js/planeta.js
@@ -1,13 +1,13 @@
 const vehiculo = document.getElementById("vehiculo");
 
 const puntos = [
-  { el: document.getElementById("punto-1"), top: "15%", left: "12%" },
-  { el: document.getElementById("punto-2"), top: "50%", left: "78%" },
-  { el: document.getElementById("punto-3"), top: "85%", left: "20%" }
+  { el: document.getElementById("punto-1"), top: "15%", left: "12%", pagina: "punto_interes1.html" },
+  { el: document.getElementById("punto-2"), top: "50%", left: "78%", pagina: "punto_interes2.html" },
+  { el: document.getElementById("punto-3"), top: "85%", left: "20%", pagina: "punto_interes3.html" }
 ];
 
-let actual = 0;       
-let viajando = false; 
+let actual = 0;
+let viajando = false;
 
 function moverA(indice) {
   viajando = true;
@@ -15,7 +15,6 @@ function moverA(indice) {
   vehiculo.style.top = puntos[indice].top;
   vehiculo.style.left = puntos[indice].left;
 }
-
 
 vehiculo.addEventListener("transitionend", () => {
   actual = Number(vehiculo.dataset.destino);
@@ -25,12 +24,12 @@ vehiculo.addEventListener("transitionend", () => {
 
 puntos.forEach((punto, indice) => {
   punto.el.addEventListener("click", () => {
-  if (viajando) return;
+    if (viajando) return;
 
-  if (indice === actual) {
-    window.location.href = `punto_interes.html?id=${punto.el.id}`;
-  } else if (indice === actual + 1 || indice === actual - 1) {
-    moverA(indice);
-  }
-});
+    if (indice === actual) {
+      window.location.href = punto.pagina;
+    } else if (indice === actual + 1 || indice === actual - 1) {
+      moverA(indice);
+    }
+  });
 });
