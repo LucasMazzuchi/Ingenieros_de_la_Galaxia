4eeafe577c3c2a2405936deb01e4572f8e4887c5
Fernando Hugo Godoy Delgado <fgodoyde@fi.uba.ar>
2026-07-22 20:36:47 +0000

    actualizo init.sql y que entre a la tierra desde bienvenida

--- frontend/js/bienvenida.js
+++ frontend/js/bienvenida.js
@@ -1,5 +1,5 @@
 document.getElementById("btnIniciar").addEventListener("click", () => {
-  window.location.href = "page/galaxia.html";
+  window.location.href = "page/planeta.html?id=1";
 });
 
 document.getElementById("btncrear").addEventListener("click", () => {
