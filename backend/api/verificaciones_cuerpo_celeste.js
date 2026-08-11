7d10a0a241432314673330ada872206d0726e7ac
LucasMazzuchi <lmazzuchi@fi.uba.ar>
2026-08-11 11:14:56 -0300

    Las verificaciones están documentadas

--- backend/api/verificaciones_cuerpo_celeste.js
+++ backend/api/verificaciones_cuerpo_celeste.js
@@ -2,6 +2,10 @@ import * as constantes from "../constantes.js";
 import {validarEntrada, validarString, validarEntero, validarBool, validarFloat,
     validarFiltros, validarValorFiltro, validarRango, orden} from "./validaciones_errores.js";
 
+
+// La función inicializa el diccionario de validadores y el de entrada para la entidad cuerpos celestes, valida la entrada mediante validarEntrada y llama a la
+// next. En caso de tener campos que no cumplan con los validadores, responde con un error 400 que contiene los campos inválidos y sus errores. Si ocurre un error
+// dentro de las validaciones, responde con un código 400 y los errores.
 export const validarCuerpoCeleste = (req, res, next) => {
     if (!req.body || Object.keys(req.body).length === 0) {
         return res.status(400).json({ error: constantes.ERROR_BODY_VACIO });
@@ -45,6 +49,10 @@ export const validarCuerpoCeleste = (req, res, next) => {
         next();
 };
 
+
+// La función inicializa los diccionarios con los validadores y los campos permitidos para filtrar en la entidad cuerpos celestes, valida que los filtros
+// dentro de req sean correctos, los procesa, los carga en body y llama a next. En caso de haber filtros que no cumplen con los validadores, responde con un
+// código 400, los filtros equivocados y sus errores.
 export const validarFiltrosCuerpoCeleste = (req, res, next) => {
     // 1. Armamos la regex dinámica para las columnas válidas de ordenamiento
     const regex = [constantes.ID, constantes.NOMBRE, constantes.TIPO, 
