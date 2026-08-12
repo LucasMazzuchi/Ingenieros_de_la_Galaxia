37103cfac0dc0cfe5b71e7fe31051f804dd212d4
LucasMazzuchi <lmazzuchi@fi.uba.ar>
2026-08-12 13:35:51 -0300

    Modularizo progreso y sus verificaciones

--- backend/api/progreso.js
+++ backend/api/progreso.js
@@ -1,19 +1,16 @@
 import { Router } from "express";
-import {validarIds, verificarEstadoPunto} from "./verificaciones_progreso.js";
-import { validarId } from "./validaciones_errores.js";
+import { validarIdsParams, validarIdsPatchCompletar, validarIdsPatchGeneral, validarReglasDesbloqueo, verificarEstadoPunto} from "./verificaciones_progreso.js";
+import { manejarError, validarId } from "./validaciones_errores.js";
 import * as progreso from "../bd/progreso.js";
-import { getVehiculo } from "../bd/vehiculos.js";
 import { completarPunto } from "../logica/puntos_interes.js";
 import {mejorarVehiculo} from "../logica/vehiculos.js";
-import { getPunto, getAllPuntos } from "../bd/puntos_interes.js";
-import { ORDER, ORDER_BY } from "../constantes.js";
 
 export const endpointsProgreso = Router();
 
 // El endpoint responde con un código 200, los puntos visitados, y el estado del planeta 
 // (completado o en progreso) asociados al vehículo y cuerpo celeste pasados por req.params. 
 // En caso de ocurrir un error, responde con un código 500 y el mensaje de error.
-endpointsProgreso.get("/:id/:cuerpo_celeste_id", validarIds, async (req, res) => {
+endpointsProgreso.get("/:id/:cuerpo_celeste_id", validarIdsParams, async (req, res) => {
     try {
         const puntosVisitados = await progreso.getAllPuntos(req.params.id, req.params.cuerpo_celeste_id);
         const resPlaneta = await progreso.getPlaneta(req.params.id, req.params.cuerpo_celeste_id);
@@ -25,41 +22,16 @@ endpointsProgreso.get("/:id/:cuerpo_celeste_id", validarIds, async (req, res) =>
         res.status(500).json({ error: "Error al obtener el progreso del vehículo." });
     }
 });
+
 // El endpoint desbloquea un punto de interés para el vehículo asociado al id pasado por req.params 
 // y responde con un código 200 y un mensaje de éxito. Si se intenta saltar a un punto que se diferencia
 // en más de 1 con su posición entre la actual y la deseada, responde con un código 409 y el mensaje de error.
 // Si el punto anterior está bloqueado, responde con un código 403 y el error.
 // Si el punto ya estaba desbloqueado, responde con un código 200. 
 // En caso de ocurrir un error interno, responde con un código 500 y el mensaje de error.
-endpointsProgreso.patch("/:id/desbloquear", validarId, validarIds, async (req, res) => {
+endpointsProgreso.patch("/:id/desbloquear", validarId, validarIdsPatchGeneral, validarReglasDesbloqueo, async (req, res) => {
     try {
-        const actual = await getPunto(req.body.punto_interes_id);
-        const vehiculo = await getVehiculo(req.params.id);
-        const puntosInteres = await getAllPuntos({
-            cuerpo_celeste_id : req.body.cuerpo_celeste_id,
-            [ORDER_BY] : "posicion",
-            [ORDER] : "ASC"
-        });
-        // Chequear que exista actual, vehículo y puntosInterés, sino 404 con un msj de error dinámico.
-        // Armar un middleware en lógica puntos de interés.
-        if (puntosInteres.length > 2 && Math.abs(vehiculo.punto_interes-actual.posicion)>1){
-                return res.status(409).json({error : "No podés saltar a este punto, debés ir a uno más cercano para poder ir a este."});
-        }
-        const actualEstado = await progreso.getPunto(req.params.id, req.body.punto_interes_id);
-        if (actualEstado){
-            return res.status(200).json({error : ""})             
-        }
-        if (actual.posicion > 1 && puntosInteres[0].id !== req.body.punto_interes_id) {
-            const puntoInteresAnterior = await progreso.getPuntoAnteriorEnPlaneta(req.body.cuerpo_celeste_id, actual.posicion);
-            console.log(puntoInteresAnterior); // Sacar
-            const estaDesbloqueada = await progreso.getPunto(req.params.id, puntoInteresAnterior.id);
-            if (!estaDesbloqueada) {
-                return res.status(403).json({ error: "No podés desbloquear este punto porque el anterior está bloqueado." });
-            }
-        }
-        // Dejar afuera del middleware de acá para abajo.
-        const yaVisitado = await progreso.getPlaneta(req.params.id, req.body.cuerpo_celeste_id);
-        if (actual.posicion === puntosInteres[0].posicion && !yaVisitado){ // Pasar esto por el body con la verificación ya hecha como un bool 'primero'
+        if (req.primero){
             const planetaVisitando = await progreso.agregarPlaneta(req.params.id, req.body.cuerpo_celeste_id);
         }
         const resPuntoInteres = await progreso.desbloquearPunto(req.params.id, req.body.punto_interes_id, req.body.cuerpo_celeste_id);
@@ -73,7 +45,7 @@ endpointsProgreso.patch("/:id/desbloquear", validarId, validarIds, async (req, r
 // El endpoint explora un punto de interés asociado al id del vehículo pasado por req.params 
 // y responde con un código 200, un mensaje de éxito, el combustible de recompensa y si el planeta 
 // fue completado. En caso de ocurrir un error, responde con un código 500 y el mensaje de error.
-endpointsProgreso.patch("/:id/explorar", validarId, validarIds, verificarEstadoPunto, completarPunto, mejorarVehiculo, async (req, res) => {
+endpointsProgreso.patch("/:id/explorar", validarId, validarIdsPatchGeneral, verificarEstadoPunto, completarPunto, mejorarVehiculo, async (req, res) => {
     try {
         const ok = await progreso.completarPlaneta(req.params.id, req.body.cuerpo_celeste_id);
         res.status(200).json({ mensaje: "¡Punto explorado con éxito!", combustible: req.body.recompensa, cuerpoCompletado: ok});
@@ -86,9 +58,9 @@ endpointsProgreso.patch("/:id/explorar", validarId, validarIds, verificarEstadoP
 // El endpoint marca un planeta como completado, otorga 100 de combustible al vehículo asociado 
 // al id pasado por req.params y responde con un código 200, un mensaje de éxito y un booleano 
 // de estado. En caso de ocurrir un error, responde con un código 500 y el mensaje de error.
-endpointsProgreso.patch("/:id/completar", validarId, validarIds, mejorarVehiculo, async (req, res) => {
+endpointsProgreso.patch("/:id/completar", validarId, validarIdsPatchCompletar, mejorarVehiculo, async (req, res) => {
     try {
-        const combustible = await progreso.sumarCombustible(req.params.id, 100); // Ver si se puede sacar sin influír en el código.
+        const combustible = await progreso.sumarCombustible(req.params.id, 100);
         const ok = await progreso.completarPlaneta(req.params.id, req.body.cuerpo_celeste_id); // Verificar que exista el planeta
         res.status(200).json({mensaje: "Planeta completado!", cuerpoCompletado: ok});
     } catch (error){
@@ -100,12 +72,12 @@ endpointsProgreso.patch("/:id/completar", validarId, validarIds, mejorarVehiculo
 // El endpoint agrega un cuerpo celeste al progreso de exploración del vehículo asociado a los 
 // ids pasados por req.params y responde con un código 200 y un mensaje de éxito. 
 // En caso de ocurrir un error, responde con un código 500 y el mensaje de error.
-endpointsProgreso.post("/:id/:cuerpo_celeste_id/agregar", validarIds, async (req,res) => {
+endpointsProgreso.post("/:id/:cuerpo_celeste_id/agregar", validarIdsParams, async (req,res) => {
     try{
-        const ok = await progreso.agregarPlaneta(req.params.id, req.params.cuerpo_celeste_id); // Chequear si existe el cuerpo asociado al id preguntando si está ok
+        const ok = await progreso.agregarPlaneta(req.params.id, req.params.cuerpo_celeste_id);
         res.status(200).json({mensaje: "Planeta Agregado!", cuerpoAgregado: ok});
-    }   catch(error) {
-        console.log("Error en agregar: ", error); // Sacar
-        res.status(500).json({error: "Error interno al querer agregar el planeta."}); // Usar manejar error para que si no existe un cuerpo o un vehículo asociado a la fk devuelva el error correcto.
+    }   catch(err) {
+        const { estado, msjError } = manejarError(err);
+        res.status(estado).json({error: msjError});
     }
 })
